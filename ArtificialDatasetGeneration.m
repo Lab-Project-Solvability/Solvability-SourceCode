5af9a69da8bcca4b406ba8d506a4b662d69ec14c
Peanut58 <1090709@students.wits.ac.za>
2020-08-25 09:15:36 +0200

    Initial Immanent Dataset .m file

--- ArtificialDatasetGeneration.m
+++ ArtificialDatasetGeneration.m
@@ -1,16 +1,33 @@
-%% Create Rectangular Cuboids
+%% Create Immanent Dataset 
 
 % Generate random numbers between 0 and 70 for the rectangles and Struct
 % Generation:
 
-numShapes = 60; 
+numShapes = 60;
 shapeCollection = [];
+shapeType = ["Rectangular Cuboid", "Cube"];
 
-for i = 1:
-    shapeCollection(i).form = 'Rectangular Cuboid';
-    shapeCollection(i).length = 70 .* rand();
-    shapeCollection(i).width = 70 .* rand();
-    shapeCollection(i).height = 70 .* rand();
-    shapeCollection(i).targetVolume = shapeCollection(i).length*shapeCollection(i).width*shapeCollection(i).height ;
+for i = 1:numShapes
+    % Generate a random number to choose the shapeType
+    typeIdx = randi(2);
+    shapeCollection(i).form = shapeType(typeIdx);
     
+    
+    
+    if shapeCollection(i).form == 'Rectangular Cuboid'
+        shapeCollection(i).length = randi(40);
+        shapeCollection(i).width = randi(40);
+        shapeCollection(i).height = randi(40);
+        shapeCollection(i).targetVolume = shapeCollection(i).length*shapeCollection(i).width*shapeCollection(i).height;
+        
+    elseif shapeCollection(i).form == 'Cube'
+        shapeCollection(i).length = randi(40);
+        shapeCollection(i).width = shapeCollection(i).length;
+        shapeCollection(i).height = shapeCollection(i).length;
+        shapeCollection(i).targetVolume = shapeCollection(i).length*shapeCollection(i).width*shapeCollection(i).height;
+    end
+
 end
+
+
+
