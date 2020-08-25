b290b3cf23349bc3f1016a06f81c8e13aacc681a
Peanut58 <1090709@students.wits.ac.za>
2020-08-25 09:39:45 +0200

    Improved Immanent Dataset

--- ArtificialDatasetGeneration.m
+++ ArtificialDatasetGeneration.m
@@ -3,7 +3,7 @@
 % Generate random numbers between 0 and 70 for the rectangles and Struct
 % Generation:
 
-numShapes = 60;
+numShapes = 100;
 shapeCollection = [];
 shapeType = ["Rectangular Cuboid", "Cube"];
 
@@ -11,17 +11,15 @@ for i = 1:numShapes
     % Generate a random number to choose the shapeType
     typeIdx = randi(2);
     shapeCollection(i).form = shapeType(typeIdx);
-    
-    
-    
+      
     if shapeCollection(i).form == 'Rectangular Cuboid'
-        shapeCollection(i).length = randi(40);
-        shapeCollection(i).width = randi(40);
-        shapeCollection(i).height = randi(40);
+        shapeCollection(i).length = randi(20);
+        shapeCollection(i).width = randi(20);
+        shapeCollection(i).height = randi(20);
         shapeCollection(i).targetVolume = shapeCollection(i).length*shapeCollection(i).width*shapeCollection(i).height;
         
     elseif shapeCollection(i).form == 'Cube'
-        shapeCollection(i).length = randi(40);
+        shapeCollection(i).length = randi(20);
         shapeCollection(i).width = shapeCollection(i).length;
         shapeCollection(i).height = shapeCollection(i).length;
         shapeCollection(i).targetVolume = shapeCollection(i).length*shapeCollection(i).width*shapeCollection(i).height;
